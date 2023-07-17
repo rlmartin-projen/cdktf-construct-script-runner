3b21611193cb0d87a43602a8e91633bf508af312
Ryan Martin <ryan@vestahealthcare.com>
2023-07-17 17:35:31 -0400

    chore: add release

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,30 @@
 import { cdktf } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
+const majorVersion = 0;
+const nodeVersion = "18";
+
 const project = new cdktf.ConstructLibraryCdktf({
   author: 'Ryan Martin',
   authorAddress: 'rlmartin@gmail.com',
-  cdktfVersion: '^0.13.0',
+  cdktfVersion: '^0.17.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.0.0',
-  name: 'cdktf-construct-script-runner',
+  name: '@rlmartin-projen/cdktf-construct-script-runner',
   projenrcTs: true,
   repositoryUrl: 'git@github.com:rlmartin-projen/cdktf-construct-script-runner.git',
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  minNodeVersion: `${nodeVersion}.0.0`,
+  workflowNodeVersion: nodeVersion,
+  majorVersion,
+  releaseBranches: {
+    dev: { prerelease: "dev", npmDistTag: "dev", majorVersion },
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ["main"],
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
